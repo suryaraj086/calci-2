32583b7e2934b40c77d8c41f5fa6c006ec9bdc35
suryaraj086 <79685586+suryaraj086@users.noreply.github.com>
2022-03-22 12:18:30 +0530

    Add files via upload

--- JSTask/src/main/webapp/script/myscript.js
+++ JSTask/src/main/webapp/script/myscript.js
@@ -297,7 +297,7 @@ function calculate(str){
 			}
             break;	
 			}
-        };
+      };
    	 if(a==arr.length-1)
 	 {
 		return arr[a];
@@ -316,3 +316,157 @@ function numberFormat(input){
     return str.join(".");
 	
 }
+
+
+    function evaluate(expression)
+    {
+//  expression="12-6+3";
+
+        let tokens = expression.split('');
+  
+         // Stack for numbers: 'values'
+        let values = [];
+  
+        // Stack for Operators: 'ops'
+        let ops = [];
+  
+        for (let i = 0; i < tokens.length; i++)
+        {
+             // Current token is a whitespace, skip it
+            if (tokens[i] == ' ')
+            {
+                continue;
+            }
+  
+            // Current token is a number,
+            // push it to stack for numbers
+            if (tokens[i] >= '0' && tokens[i] <= '9')
+            {
+                let sbuf = "";
+                  
+                // There may be more than
+                // one digits in number
+                while (i < tokens.length &&
+                        tokens[i] >= '0' &&
+                            tokens[i] <= '9')
+                {
+                    sbuf = sbuf + tokens[i++];
+                }
+                values.push(parseInt(sbuf, 10));
+                
+                // Right now the i points to
+                // the character next to the digit,
+                // since the for loop also increases
+                // the i, we would skip one
+                //  token position; we need to
+                // decrease the value of i by 1 to
+                // correct the offset.
+                  i--;
+            }
+  
+            // Current token is an opening
+            // brace, push it to 'ops'
+            else if (tokens[i] == '(')
+            {
+                ops.push(tokens[i]);
+            }
+  
+            // Closing brace encountered,
+            // solve entire brace
+            else if (tokens[i] == ')')
+            {
+                while (ops[ops.length - 1] != '(')
+                {
+                  values.push(applyOp(ops.pop(),
+                                   values.pop(),
+                                  values.pop()));
+                }
+                ops.pop();
+            }
+  
+            // Current token is an operator.
+            else if (tokens[i] == '+' ||
+                     tokens[i] == '-' ||
+                     tokens[i] == '*' ||
+                     tokens[i] == '/')
+            {
+                  
+                // While top of 'ops' has same
+                // or greater precedence to current
+                // token, which is an operator.
+                // Apply operator on top of 'ops'
+                // to top two elements in values stack
+                while (ops.length > 0 &&
+                         hasPrecedence(tokens[i],
+                                     ops[ops.length - 1]))
+                {
+                  values.push(applyOp(ops.pop(),
+                                   values.pop(),
+                                 values.pop()));
+                }
+  
+                // Push current token to 'ops'.
+                ops.push(tokens[i]);
+            }
+        }
+  
+        // Entire expression has been
+        // parsed at this point, apply remaining
+        // ops to remaining values
+        while (ops.length > 0)
+        {
+            values.push(applyOp(ops.pop(),
+                             values.pop(),
+                            values.pop()));
+        }
+  
+        // Top of 'values' contains
+        var out=values.pop()
+console.log(out)
+        return out;
+    }
+  
+    // Returns true if 'op2' has
+    // higher or same precedence as 'op1',
+    // otherwise returns false.
+    function hasPrecedence(op1, op2)
+    {
+        if (op2 == '(' || op2 == ')')
+        {
+            return false;
+        }
+        if ((op1 == '*' || op1 == '/') &&
+               (op2 == '+' || op2 == '-'))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+  
+    // A utility method to apply an
+    // operator 'op' on operands 'a'
+    // and 'b'. Return the result.
+    function applyOp(op, b, a)
+    {
+        switch (op)
+        {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            if (b == 0)
+            {
+                document.write("Cannot divide by zero");
+            }
+            return parseInt(a / b, 10);
+        }
+        return 0;
+    }
+     
+
